c15b9eee52cc0e2d5b1fa74d6be5e4d381b45f8e
gljivar <gljivar@gmail.com>
2013-03-12 00:23:29 +0100

    Minor - found about reload method of model for translations. Inverse translations don't work

--- app/models/language_object.rb
+++ app/models/language_object.rb
@@ -7,5 +7,9 @@ class LanguageObject < ActiveRecord::Base
   has_many :translations
   has_many :language_objects, :through => :translations
 
+  #FIXME: Not working
+  has_many :inverse_translations, :class_name => "Translation", :foreign_key => "translation_id"
+  has_many :inverse_language_objects, :through => :inverse_translations, :source => :language_object
+
   attr_accessible :meaning, :text, :translations
 end
