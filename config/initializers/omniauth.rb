d363a45b5038c41ea32b027b376ba0e6054c6f79
gljivar <gljivar@gmail.com>
2013-03-18 20:33:56 +0100

    Added Google oAuth, possibly WindowsLive

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '431829256892441', '4cf34fd43e4f00efa00fe8eed2c14a52'
   provider :google_oauth2, '685175429521.apps.googleusercontent.com', 'Gkz-EIo1KRbWPBqloQB8RY0m',
     { :approval_prompt => "auto" }
-  provider :windowslive, '00000000480EC427', 'Kd9W0cX5QjiN2cOBNzrfMbKWDVxz0QD7'
+  provider :windowslive, '00000000480EC427', 'Kd9W0cX5QjiN2cOBNzrfMbKWDVxz0QD7', :scope => 'wl.basic'
 end
