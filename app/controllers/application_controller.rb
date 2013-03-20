612121373068d4b16847dfbe1f792a4a212b9cca
gljivar <gljivar@gmail.com>
2013-03-20 21:33:30 +0100

    Added login requirement accross all actions except sessions index, sessions create and home index.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :require_login
 	
   helper_method :current_user
 
   private
 
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to access this section"
+      respond_to do |format|
+        format.html { redirect_to :signin,
+          :notice => "You must be signed in to access this page" }
+        format.json { render :json => {:success => false, :message => 'You must be signed in to access this page.'}, :status => :unauthorized }
+      end
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+
 end
