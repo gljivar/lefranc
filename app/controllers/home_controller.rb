9a053879e152d897bddac875c2291b0d3bfe4198
gljivar <gljivar@gmail.com>
2013-03-30 19:33:58 +0100

    Change home page. It now shows top 5 groups by joining in of user. Better to implement some activity metrics.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,14 @@ class HomeController < ApplicationController
   skip_before_filter :require_login, :only => [:index]
 
   def index
+    #TODO: Take two most recent groups that a user belongs to, that has some language object or lesson activity
+    #TODO: Exclude duplicates
+    #Now: Take two groups you most recently entered
+
+    @group_users = GroupUser.where(:user_id => current_user.id).order('created_at desc').limit(5)
+    @groups = []
+    @group_users.each do |group_user| @groups.push(Group.find(group_user.group_id)) end
+    
+    #TODO: Take lessons with some recent activity
   end
 end
