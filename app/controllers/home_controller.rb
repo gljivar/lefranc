474c0d30376ca08b05022186d40450a5aa738f91
gljivar <gljivar@gmail.com>
2013-04-05 00:15:19 +0200

    Fix group view, and home lessons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,13 +8,13 @@ class HomeController < ApplicationController
 
     #TODO: This is hotfix
     if current_user
-    @group_users = GroupUser.where(:user_id => current_user.id).order('created_at desc').limit(5)
-    @groups = []
-    @group_users.each do |group_user| @groups.push(Group.find(group_user.group_id)) end
+      @group_users = GroupUser.where(:user_id => current_user.id).order('created_at desc').limit(5)
+      @groups = []
+      @group_users.each do |group_user| @groups.push(Group.find(group_user.group_id)) end
     
-    #TODO: Take lessons with some recent activity
-    @lessons =[]
-    #@groups.each do |group| @lessons.push(Lesson.where(:group_id => group.id)) end
+      #TODO: Take lessons with some recent activity
+      @lessons =[]
+      @groups.each do |group| @lessons = @lessons + Lesson.where(:group_id => group.id) end
     end 
   end
 end
